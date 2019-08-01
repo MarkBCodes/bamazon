ff871f16e03e34a028b583636e0e50bea3539fdd
Mark B <mark.bullard42@gmail.com>
2019-08-01 14:39:50 -0400

    product display code added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,3 +16,27 @@ dbConnect.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + dbConnect.threadId);
 });
+// product display
+var productDisplay = function() {
+  var query = "SELECT * FROM products";
+  dbConnect.query(query, function(err, res) {
+    if (err) throw err;
+    var tableDisplay = new Table({
+      head: ["Item Id", "Product Name", "Category", "Price", "Quantity"],
+      colWidths: [10, 25, 25, 10, 14]
+    });
+    for (var i = 0; i < res.length; i++) {
+      displayTable.push([
+        res[i].item_id,
+        res[i].product_name,
+        res[i].department_name,
+        res[i].price,
+        res[i].stock_quantity
+      ]);
+    }
+    console.log(displayTable.toString());
+    purchasePrompt();
+  });
+};
+
+// prompt for quantity and purchase
